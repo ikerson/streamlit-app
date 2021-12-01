6d0ca4633825c5ee8b2c9323204ecdb33c3df561
ikerson <35290674+ikerson@users.noreply.github.com>
2021-12-01 19:51:43 +0900

    app version 3

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ local_css("style.css")
 
 #Load dataframe
 
-pred_df = pd.read_csv("./data/predictions_output.csv")
+pred_df = pd.read_csv("./data/predictions_output_final.csv")
 
 #Declare values
 
@@ -25,13 +25,16 @@ race_dropdown_vals = ["Hispanic", "White", "Black", "Asian", "Other"]
 
 internet_dropdown_vals = ["Never", "Rarely", "Sometimes", "Usually", "Always"]
 
+computer_dropdown_vals = ["Never", "Rarely", "Sometimes", "Usually", "Always"]
+
 target_hours_val = 4
 
-def seg_val_tracker(race_val, income_val, internet_val, target_hours_val):
+def seg_val_tracker(race_val, income_val, internet_val, computer_val, target_hours_val):
     new_seg_val = pred_df[(pred_df["RACE_ETHNICITY"] == race_val) \
     & (pred_df["INCOME"] == income_slider_vals.index(income_val) + 1)\
     & (pred_df["INTRNTAVAIL"] == internet_dropdown_vals[::-1].index(internet_val) + 1)\
-    & (pred_df["SCHLHRS"] == target_hours_val)].iloc[:,[5]].values.astype(float)
+    & (pred_df["COMPAVAIL"] == computer_dropdown_vals[::-1].index(computer_val) + 1)\
+    & (pred_df["SCHLHRS"] == target_hours_val)].iloc[:,[6]].values.astype(float)
     return float(new_seg_val * 100)
 
 right_bar_color = "#3182bd"
@@ -53,18 +56,22 @@ with col1:
     with st.form("segment_form"):
         st.subheader("Student Traits")
         race_val = st.selectbox("Race", race_dropdown_vals, 
-                        help="select a value")
+                        help="Select the demographic to see how it effects the likelihood.")
         income_val = st.select_slider("Income",
                         options = income_slider_vals,
                         value = "50-75k",
-                        help = "select a value")
+                        help = "Change the household income to see how it effects the likelihood.")
         internet_val = st.select_slider("Internet availability",
                         options = internet_dropdown_vals,
                         value="Sometimes",
-                        help = "select a value")
+                        help = "Change internet availability to see how it effects the likelihood.")
+        computer_val = st.select_slider("Computer availability",
+                        options = internet_dropdown_vals,
+                        value="Sometimes",
+                        help = "Change computer availability to see how it effects the likelihood.")
         submitted = st.form_submit_button("Submit")
 
-new_seg_val = seg_val_tracker(race_val, income_val, internet_val, target_hours_val)
+new_seg_val = seg_val_tracker(race_val, income_val, internet_val, computer_val, target_hours_val)
 
 with col2:
     
@@ -81,8 +88,9 @@ with col2:
                     '''
     st.markdown(segement_html, unsafe_allow_html=True)
     lower_internet_val = internet_val.lower()
+    lower_computer_val = computer_val.lower()
     segment_likelihood = f'''<html>
-                                <p style="color:{right_bar_color}; font-size:20px; text-align:center;">Likelihood that <mark>{race_val}s</mark> who <mark>{lower_internet_val}</mark> have internet avalibilty and live in a household with an income of <mark>{income_val}</mark> had virtual contact with a teacher <mark>four or more times</mark> per week.</p>
+                                <p style="color:{right_bar_color}; font-size:20px; text-align:center;">Likelihood that <mark>{race_val}s</mark> who <mark>{lower_internet_val}</mark> have internet, <mark>{lower_computer_val}</mark> have a computer, and live in a household with an income of <mark>{income_val}</mark> had virtual contact with a teacher <mark>four or more times</mark> per week.</p>
                             </html>
                         '''
     st.markdown(segment_likelihood, unsafe_allow_html=True)
